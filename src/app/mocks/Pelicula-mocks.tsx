fd16c17e218519552b93c9abb5db2b4c0fe43744
Ernesto <ediaz41@cuc.edu.co>
2024-11-18 10:44:43 -0500

    peliculasproject

---
+++ src/app/mocks/Pelicula-mocks.tsx
@@ -0,0 +1,12 @@
+import { Pelicula } from "../modelos/Pelicula";
+
+export const ARREGLO_PELICULAS:Array<Pelicula>=[
+    new Pelicula(1,"Misión Imposible", "Tom Cruise","1","",""),
+    new Pelicula(1,"Jurassic Park", "Sam Neill","2","",""),
+    new Pelicula(1,"El Justiciero", "Denzel Washington","1","",""),
+    new Pelicula(1,"Dune", "Timotheé Chalamet","3","",""),
+    new Pelicula(1,"Zombieland", "Emma Stone","1","",""),
+    new Pelicula(1,"El Conjuro", "Patrick Wilson","1","",""),
+
+
+]
